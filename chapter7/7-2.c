a219eca007d6d93e538dda23edeeeb1ccaca5e10
HoJun Heo <116245224+HeoHoJun@users.noreply.github.com>
2023-12-16 11:35:28 +0900

    매개변수가 없는 함수 (음수가 입력되면 입력 과정 반복)

--- chapter7/7-2.c
+++ chapter7/7-2.c
@@ -17,6 +17,11 @@ int get_num(void)                               // 매개변수가 없고 반환형만 있다
 
 	printf("양수 입력 : ");                     // 입력 안내 메시지
 	scanf("%d", &num);                          // 키보드 입력
-
+	while (num < 0)                             // 움수가 입력되면 입력 과정 반복
+	{
+		printf("양수를 입력하세요!\n");
+		printf("양수 입력 : ");
+		printf("%d", &num);
+	}
 	return num;                                 // 입력한 값 반환
 }
